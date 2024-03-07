e50506b9d86aa4cfe5e8db391415acf2dd6a4da9
ValeriaLukovich <valeriialukovich@Valeriias-MacBook-Air.local>
2024-03-07 11:29:02 +0100

    last lesson

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
-from hexlet_django_blog.article.views import IndexView, ArticleView, ArticleCommentsView
+from hexlet_django_blog.article.views import (
+    IndexView,
+    ArticleView,
+    ArticleCommentsView,
+    ArticleFormCreateView,
+    ArticleFormEditView,
+    ArticleFormDeleteView
+)
 from hexlet_django_blog.article import views
 
 urlpatterns = [
     path('', IndexView.as_view()),
+    path('<int:id>/edit/', ArticleFormEditView.as_view(), name='articles_update'),
+    path('<int:id>/delete/', ArticleFormDeleteView.as_view(), name='articles_delete'),
     path('<int:id>/', ArticleView.as_view(), name='article_detail'),
     path('<int:article_id>/comments/<int:id>/', ArticleCommentsView.as_view()),
+    path('create/', ArticleFormCreateView.as_view(), name='articles_create'),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -3,6 +3,8 @@ from django.views import View
 from django.http import HttpResponse
 from django.shortcuts import redirect, reverse
 from hexlet_django_blog.article.models import Article
+from .forms import CommentArticleForm
+from hexlet_django_blog.article.forms import ArticleForm
 
 NAME_APP = 'Hexlet-Django-Blog'
 
@@ -32,3 +34,70 @@ class ArticleCommentsView(View):
         return render(request, 'articles/article.html', context={
             'article': article
         })
+
+
+class CommentArticleView(View):
+
+    def get(self, request, *args, **kwargs):
+        form = CommentArticleForm()
+        return render(request, 'comment.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = CommentArticleForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                name = form.cleaned_data['content'], 
+                )
+            comment.save()
+
+
+class ArticleCommentFormView(View):
+
+    def post(self, request, *args, **kwargs):
+        form = ArticleCommentForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+
+class ArticleFormCreateView(View):
+
+    def get(self, request, *args, **kwargs):
+        form = ArticleForm()
+        return render(request, 'articles/create.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('articles')
+        return render(request, 'articles/create.html', {'form': form})
+
+
+class ArticleFormEditView(View):
+
+    def get(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        form = ArticleForm(instance=article)
+        return render(request, 'articles/update.html', {'form': form, 'article_id':article_id})
+
+
+def post(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('articles')
+
+        return render(request, 'articles/update.html', {'form': form, 'article_id':article_id})
+
+
+class ArticleFormDeleteView(View):
+
+    def post(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        if article:
+            article.delete()
+        return redirect('articles')
